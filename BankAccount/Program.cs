6167a65597be43e43f29fa667dfb51e5b842e5ad
jianyik96 <jianyi96kang@gmail.com>
2021-05-31 13:25:03 +0800

    Add project files.

---
+++ BankAccount/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    //It has a 10-digit number that uniquely identifies the bank account.
+    //It has a string that stores the name or names of the owners.
+    //The balance can be retrieved.
+    //It accepts deposits.
+    //It accepts withdrawals.
+    //The initial balance must be positive.
+    //Withdrawals cannot result in a negative balance.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch(ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch(InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+            Console.WriteLine(account.GetAccountHistory());
+
+            Console.ReadLine();
+        }
+    }
+}
